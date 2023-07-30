df9a737ac1ef702c0571516a312fbc971a109edf
Chukwuemeka-Ike <chukwuemeka.o.ike@gmail.com>
2023-07-30 19:40:18 -0400

    Update operator_gui.py
    Implemented machine choice and ticket population based on that.

--- ws/src/arm_gui/src/operator_gui/operator_gui.py
+++ ws/src/arm_gui/src/operator_gui/operator_gui.py
@@ -13,18 +13,28 @@ import rospkg
 import sys
 import threading
 
-from geometry_msgs.msg import Pose2D, Twist, PoseStamped
-from rviz import bindings as rviz
-from tf import TransformListener
+from geometry_msgs.msg import Twist, PoseStamped
+from std_msgs.msg import UInt32
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import *
 
+from tf import TransformListener
+
+from arm_msgs.msg import Ticket, Tickets
+from arm_msgs.srv import MachineStatus, MachineStatusRequest,\
+    TicketList, TicketListRequest,\
+    UnboundMachines, UnboundMachinesRequest
+
+from arm_utils.conversion_utils import convert_ticket_list_to_task_dict
+
 from gui_common.gui_elements import ControlToggleButton, RobotButton, FixedWidthLabel
 from gui_common.dialogs import TicketInfoDialog
 from gui_common.map_viz import create_map_widget
 
+
+log_tag = "Operator GUI"
 
 callback_lock = threading.Lock()
 structureButtonFontSize = 20
@@ -37,9 +47,9 @@ class OperatorGUI(QMainWindow):
         self.setWindowTitle("Operator GUI")
 
         rospy.init_node("operator_gui")
-        rospy.loginfo("Initializing Operator GUI")
-        rp = rospkg.RosPack()
-        self.gui_path = rp.get_path("arm_gui")
+        rospy.on_shutdown(self.shutdown_gui)
+        rospy.loginfo(f"{log_tag}: Node started.")
+
         # RViz configuration location.
         rviz_folder = os.path.join(
             rospkg.RosPack().get_path("arm_gui"), 'rviz'
@@ -47,7 +57,6 @@ class OperatorGUI(QMainWindow):
         self.rviz_path = rospy.get_param(
             "gui_rviz_path", os.path.join(rviz_folder, "config.rviz")
         )
-        rospy.on_shutdown(self.shutdownGUI)
 
         self.ticketID = 4
 
@@ -58,7 +67,53 @@ class OperatorGUI(QMainWindow):
         self.overallLayout = QVBoxLayout(self.centralWidget)
         self.centralWidget.setLayout(self.overallLayout)
 
+        # Publishers for binding/releasing a machine to/from the GUI instance.
+        self.bind_machine_pub = rospy.Publisher(
+            "bind_machine", UInt32, queue_size=10
+        )
+        self.release_machine_pub = rospy.Publisher(
+            "release_machine", UInt32, queue_size=10
+        )
+        # Publisher for starting a ticket.
+        self.start_ticket_pub = rospy.Publisher(
+            "start_ticket", Ticket, queue_size=10
+        )# Publisher for ending a ticket when done.
+        self.end_ticket_pub = rospy.Publisher(
+            "end_ticket", Ticket, queue_size=10
+        )
+
         # Swarm Version 1 Code.
+        # Control setup.
+        self.get_control_params()
+
+        self.buttons = []
+        self.labels = []
+
+        self.synced_control_enabled = False
+        self.rotation_disabled = False
+        self.translation_disabled = False
+        self.tf = TransformListener()
+        self.tf_changer = rospy.Publisher(self.tf_changer_topic, PoseStamped, queue_size=10)
+
+        # Subscribe to the input command topic.
+        rospy.Subscriber(self.input_command_topic, Twist, self._offsetCallback)
+
+        self.unbound_machines = []
+        self.unbound_machine_names = []
+
+        # Create the display and status bar.
+        self._createDisplay()
+        self._createStatusBar()
+
+        # Show the window.
+        self.show()
+
+    def shutdown_gui(self):
+        '''Gracefully shutdown the GUI elements. Particularly RViz.'''
+        # self.manager
+
+    def get_control_params(self):
+        '''.'''
         self.number_of_bots = rospy.get_param('number_of_robots')
         self.nodenames = rospy.get_param('robot_node_names')
         self.open_loop_command_topics = rospy.get_param('open_loop_command_topics')
@@ -74,44 +129,24 @@ class OperatorGUI(QMainWindow):
         self.resize_swarm_scaling_factor = float(rospy.get_param('resize_scaling_factor'))
         self.tf_changer_topic = rospy.get_param('tf_changer_topic')
 
-        self.buttons = []
-        self.labels = []
-        
-
-        # Subscribe to the input command topic.
-        rospy.Subscriber(self.input_command_topic, Twist, self._offsetCallback)
-
-        # self.end_ticket_pub = rospy.Publisher("end_ticket",)
-
-        self.synced_control_enabled = False
-        self.rotation_disabled = False
-        self.translation_disabled = False
-        self.tf = TransformListener()
-        self.tf_changer = rospy.Publisher(self.tf_changer_topic, PoseStamped, queue_size=10)
-
-        # Create the display and status bar.
-        self._createDisplay()
-        self._createStatusBar()
-
-        # Show the window.
-        self.show()
-
-    def shutdownGUI(self):
-        '''Gracefully shutdown the GUI elements. Particularly RViz.'''
-        # self.manager
-
     def _createDisplay(self):
         '''Create the basic display.'''
         
+        self.create_machine_layout()
         self._createTicketLayout()
         self._createButtonsLayout()
 
         # Create the RViz widget.
         self.create_map_widget()
 
+        self.overallLayout.addLayout(self.machineIDLayout)
         self.overallLayout.addLayout(self.currentTicketLayout)
         self.overallLayout.addLayout(self.buttonsLayout)
         self.overallLayout.addWidget(self.mapWidget)
+
+        # Disable the ticket and buttons layouts.
+        self.disable_layout(self.currentTicketLayout)
+        self.disable_layout(self.buttonsLayout)
 
     def _createStatusBar(self):
         '''Create a simple status bar.'''
@@ -119,25 +154,223 @@ class OperatorGUI(QMainWindow):
         status.showMessage("Operator GUI")
         self.setStatusBar(status)
 
+    def create_machine_layout(self):
+        '''.'''
+        self.request_unbound_machines()
+
+        componentSizes = self.width()/5
+        self.machineIDLayout = QHBoxLayout()
+        machineIDLabel = QLabel("Machine ID")
+        machineIDLabel.setAlignment(Qt.AlignCenter)
+
+        self.machineIDComboBox = QComboBox()
+        self.machineIDComboBox.addItem("Select Machine ID")
+        self.machineIDComboBox.addItems(self.unbound_machine_names)
+        self.machineIDComboBox.currentIndexChanged.connect(self.select_machine_id)
+
+        self.machineIDSelectButton = QPushButton("Set Machine")
+        self.machineIDSelectButton.setEnabled(False)
+        self.machineIDSelectButton.clicked.connect(self.bind_machine_id)
+        
+        self.machineIDReleaseButton = QPushButton("Release Machine")
+        self.machineIDReleaseButton.setStyleSheet("background-color : red")
+        self.machineIDReleaseButton.setEnabled(False)
+        self.machineIDReleaseButton.clicked.connect(self.release_machine_id)
+
+        self.machineIDLayout.addWidget(machineIDLabel)
+        self.machineIDLayout.addWidget(self.machineIDComboBox)
+        self.machineIDLayout.addWidget(self.machineIDSelectButton)
+        self.machineIDLayout.addWidget(self.machineIDReleaseButton)
+
+    def select_machine_id(self):
+        '''.'''
+        if self.machineIDComboBox.currentText() == "Select Machine ID":
+            self.machineIDSelectButton.setEnabled(False)
+        else:
+            self.machineIDSelectButton.setEnabled(True)
+
+    def bind_machine_id(self):
+        '''Binds the Operator GUI to the selected machine ID.
+
+        Sends the machine ID message over the topic, which Machine Manager
+        subscribes to.
+        '''
+        # Publish the message.
+        msg = UInt32()
+        machine_id = self.unbound_machines[
+            self.unbound_machine_names.index(
+                self.machineIDComboBox.currentText()
+            )
+        ]
+        msg.data = machine_id
+        self.bind_machine_pub.publish(msg)
+
+        # Disable the select button and combo box, and enable the release button.
+        self.machineIDComboBox.setEnabled(False)
+        self.machineIDSelectButton.setEnabled(False)
+        self.machineIDReleaseButton.setEnabled(True)
+
+        # Populate the ticket combo box with the IDs that are assigned to the
+        # machine and ready.
+        self.request_machine_assigned_tickets()
+        self.request_ticket_list()
+        self.ready_assigned_tickets = [
+            id for id in self.assigned_tickets if id in self.ready
+        ]
+        self.ticketIDComboBox.addItems([
+            str(id) for id in self.ready_assigned_tickets
+        ])
+
+        # Enable the ticket and buttons layouts.
+        self.enable_layout(self.currentTicketLayout)
+        self.enable_layout(self.buttonsLayout)
+
+    def release_machine_id(self):
+        '''Releases the machine ID from the Operator GUI.
+
+        Sends the machine ID message over the topic, which Machine Manager
+        subscribes to.
+        '''
+        # Publish the message.
+        msg = UInt32()
+        machine_id = self.unbound_machines[
+            self.unbound_machine_names.index(
+                self.machineIDComboBox.currentText()
+            )
+        ]
+        msg.data = machine_id
+        self.release_machine_pub.publish(msg)
+
+        # Enable the select button and combo box, and disable the release button.
+        self.machineIDComboBox.setEnabled(True)
+        self.machineIDSelectButton.setEnabled(True)
+        self.machineIDReleaseButton.setEnabled(False)
+
+        # Empty the ticket combo box and the assigned ticket list.
+        self.ready_assigned_tickets = []
+        self.ticketIDComboBox.clear()
+
+        # Disable the ticket and buttons layouts.
+        self.disable_layout(self.currentTicketLayout)
+        self.disable_layout(self.buttonsLayout)
+
+    def request_unbound_machines(self):
+        '''Requests the machines that are not already bound to a GUI.'''
+        rospy.wait_for_service('unbound_machine_service')
+        try:
+            # TicketListRequest() is empty.
+            request = UnboundMachinesRequest()
+            unbound_machines = rospy.ServiceProxy('unbound_machine_service', UnboundMachines)
+            response = unbound_machines(request)
+
+            self.unbound_machines = response.machine_ids
+            self.unbound_machine_names = response.machine_names
+
+        except rospy.ServiceException as e:
+            rospy.logerr(f'{log_tag}: Unbound machines request failed: {e}.')
+
+    def request_machine_assigned_tickets(self):
+        '''Requests the tickets assigned to the selected machine ID.'''
+        try:
+            request = MachineStatusRequest()
+            request.machine_id = self.unbound_machines[
+                self.unbound_machine_names.index(
+                    self.machineIDComboBox.currentText()
+                )
+            ]
+            machine_status = rospy.ServiceProxy('machine_status_service', MachineStatus)
+            response = machine_status(request)
+
+            self.assigned_tickets = response.assigned_ids
+            self.machine_status = response.status
+        except rospy.ServiceException as e:
+            rospy.logerr(f'{log_tag}: Machine assigned tickets request failed: {e}.')
+
+    def request_ticket_list(self):
+        '''Request the current ticket list from the ticket_service.'''
+        rospy.wait_for_service('ticket_service')
+        try:
+            # TicketListRequest() is empty.
+            request = TicketListRequest()
+            ticket_list = rospy.ServiceProxy('ticket_service', TicketList)
+            response = ticket_list(request)
+
+            self.all_tickets = convert_ticket_list_to_task_dict(response.all_tickets)
+            self.waiting = response.waiting
+            self.ready = response.ready
+            self.ongoing = response.ongoing
+            self.done = response.done
+        except rospy.ServiceException as e:
+            rospy.logerr(f'{log_tag}: Ticket list request failed: {e}.')
+
     def _createTicketLayout(self):
         '''.'''
         self.currentTicketLayout = QHBoxLayout()
-        ticketLabel = QLabel("Current Ticket: ")
+        ticketLabel = QLabel("Current Ticket")
         ticketLabel.setAlignment(Qt.AlignCenter)
-        self.idButton = QPushButton(str(self.ticketID))
+
+        self.ticketIDComboBox = QComboBox()
+        self.ticketIDComboBox.currentIndexChanged.connect(self.on_ticket_id_changed)
+
+        self.idButton = QPushButton("Details")
         self.idButton.clicked.connect(self._displayJobCharacteristics)
+
+        self.startButton = QPushButton("Start")
+        self.startButton.clicked.connect(self.start_ticket)
+
         self.endButton = QPushButton("End")
         self.endButton.setStyleSheet("background-color : green")
-        
+        self.endButton.clicked.connect(self.end_ticket)
+
         self.currentTicketLayout.addWidget(ticketLabel)
+        self.currentTicketLayout.addWidget(self.ticketIDComboBox)
         self.currentTicketLayout.addWidget(self.idButton)
+        self.currentTicketLayout.addWidget(self.startButton)
         self.currentTicketLayout.addWidget(self.endButton)
+
+    def on_ticket_id_changed(self):
+        '''.'''
 
     def _displayJobCharacteristics(self):
         '''.'''
         ticketInfoDialog = TicketInfoDialog(self)
         ticketInfoDialog.setModal(True)
         ticketInfoDialog.show()
+
+    def start_ticket(self):
+        '''Starts the selected ticket and publishes its ID.'''
+        # Publish the message.
+        msg = Ticket()
+        msg.ticket_id = int(self.ticketIDComboBox.currentText())
+        self.start_ticket_pub.publish(msg)
+
+        # Disable the release machine button and the ID dropdown.
+        self.ticketIDComboBox.setEnabled(False)
+        self.machineIDReleaseButton.setEnabled(False)
+        
+    def end_ticket(self):
+        '''Ends the selected ticket and publishes its ID.'''
+        # Publish the message.
+        msg = Ticket()
+        msg.ticket_id = int(self.ticketIDComboBox.currentText())
+        self.end_ticket_pub.publish(msg)
+
+        # Enable the release machine button and the ID dropdown.
+        self.ticketIDComboBox.setEnabled(True)
+        self.machineIDReleaseButton.setEnabled(True)
+
+        # Empty the ticket combo box, then add the IDs after requesting
+        # a new ticket list and machine assignments.
+        self.request_machine_assigned_tickets()
+        self.request_ticket_list()
+
+        self.ready_assigned_tickets = [
+            id for id in self.assigned_tickets if id in self.ready
+        ]
+        self.ticketIDComboBox.clear()
+        self.ticketIDComboBox.addItems([
+            str(id) for id in self.ready_assigned_tickets
+        ])
 
     def _createButtonsLayout(self):
         '''.'''
@@ -256,6 +489,32 @@ class OperatorGUI(QMainWindow):
                 view_man.setCurrentFrom(view_man.getViewAt(i))
                 return
         print("Could not find view named {view_name}")
+
+    def enable_layout(self, layout):
+        '''Enables all the widgets in the given layout.'''
+        for i in range(layout.count()):
+            item = layout.itemAt(i)
+            if isinstance(item, QWidgetItem):
+                widget = item.widget()
+                if widget:
+                    widget.setEnabled(True)
+            elif isinstance(item, QLayoutItem):
+                sub_layout = item.layout()
+                if sub_layout:
+                    self.enable_layout(sub_layout)
+
+    def disable_layout(self, layout):
+        '''Disables all the widgets in the given layout.'''
+        for i in range(layout.count()):
+            item = layout.itemAt(i)
+            if isinstance(item, QWidgetItem):
+                widget = item.widget()
+                if widget:
+                    widget.setEnabled(False)
+            elif isinstance(item, QLayoutItem):
+                sub_layout = item.layout()
+                if sub_layout:
+                    self.disable_layout(sub_layout)
 
     def _offsetCallback(self, msg):
         '''Alters the received input command, then publishes to enabled bots.'''
