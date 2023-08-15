7fe097dfb47193e5a3b2a1a44a638d9a276ca3b1
Chukwuemeka-Ike <chukwuemeka.o.ike@gmail.com>
2023-08-14 22:16:03 -0400

    Update gui_elements.py
    Added LEDManager and created MapWidget class instead of the previous implementation method

--- ws/src/arm_gui/src/gui_common/gui_elements.py
+++ ws/src/arm_gui/src/gui_common/gui_elements.py
@@ -1,14 +1,27 @@
+#!/usr/bin/env python3
+'''
+Rensselaer Polytechnic Institute - Julius Lab
+ARM Project
+Author - Chukwuemeka Osaretin Ike
+
+Description:
+    Common GUI elements used across the Supervisor and Operator GUIs.
+'''
 import rospy
+import time
 
 from PyQt5.QtCore import QPointF, Qt, pyqtProperty
 from PyQt5.QtGui import QBrush, QColor, QPainter, QPen, QRadialGradient
 from PyQt5.QtWidgets import QAbstractButton, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QVBoxLayout
+from rviz import bindings as rviz
 
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32
 
 
-class ControlToggleButton(QPushButton):
-    '''Changes push button color when pressed.'''
+class ToggleButton(QPushButton):
+    '''A push button that changes color when pressed.'''
     def __init__(self, label):
         '''.'''
         super().__init__(label)
@@ -23,14 +36,12 @@ class ControlToggleButton(QPushButton):
         else:
             self.setStyleSheet("QPushButton {background-color: light gray; color: black;}")
 
-
-class RobotButton():
+class RobotButton(ToggleButton):
     '''.'''
     def __init__(self, button_name, button_topic) -> None:
         self.buttonName = button_name + "\n Motion Enable"
-        self.button = ControlToggleButton(self.buttonName)
+        super().__init__(self.buttonName)
         self.publisher = rospy.Publisher(button_topic, Twist, queue_size=0)
-
 
 class FixedWidthLabel(QLabel):
     '''Creates a fixed width label. Allows us specify the size in the constructor.'''
@@ -48,12 +59,100 @@ class FixedWidthLineEdit(QLineEdit):
         if width is not None:
             self.setFixedWidth(width)
 
+
 class FixedWidthPushButton(QPushButton):
     '''Creates a fixed width push button.'''
     def __init__(self, width=None):
         super().__init__()
         if width is not None:
             self.setFixedWidth(width)
+
+
+class MapWidget(QWidget):
+    '''Widget containing an RViz view for map visualization.'''
+    def __init__(self, rviz_path: str) -> None:
+        super().__init__()
+        self.frame = rviz.VisualizationFrame()
+        self.frame.setSplashPath("")
+        self.frame.initialize()
+
+        reader = rviz.YamlConfigReader()
+        config = rviz.Config()
+        reader.readFile(config, rviz_path)
+        self.frame.load(config)
+
+        self.setWindowTitle( config.mapGetChild( "Title" ).getValue() )
+
+        self.frame.setMenuBar( None )
+        self.frame.setStatusBar( None )
+        self.frame.setHideButtonVisibility( False )
+
+        self.manager = self.frame.getManager()
+
+        mapLayout = QVBoxLayout()
+        mapLayout.addWidget(self.frame)
+
+        h_layout = QHBoxLayout()
+
+        self.top_button = QPushButton("Top View")
+        self.top_button.clicked.connect(self._onTopButtonClick)
+        h_layout.addWidget(self.top_button)
+
+        self.side_button = QPushButton("Side View")
+        self.side_button.clicked.connect(self._onSideButtonClick)
+        h_layout.addWidget(self.side_button)
+
+        mapLayout.addLayout(h_layout)
+
+        self.setLayout(mapLayout)
+
+    def _onTopButtonClick(self):
+        '''.'''
+        self._switchToView("Top View")
+
+    def _onSideButtonClick(self):
+        '''.'''
+        self._switchToView("Side View")
+
+    def _switchToView(self, view_name: str):
+        '''Looks for view_name in the saved views in the config.'''
+        view_man = self.manager.getViewManager()
+        for i in range(view_man.getNumViews()):
+            if view_man.getViewAt(i).getName() == view_name:
+                view_man.setCurrentFrom(view_man.getViewAt(i))
+                return
+        print("Could not find view named {view_name}")
+
+
+class LEDManager:
+    def __init__(self,node_names,led_objects):
+        self.node_names = node_names
+        self.led_objects = led_objects
+        self.active_bots = []
+        self.robot_enable_status_topic = rospy.get_param('robot_enable_status_topic') 
+        self.publisher = rospy.Publisher(self.robot_enable_status_topic, Int32, queue_size=10)
+        self.send_value = 0
+        for i in range(len(led_objects)):
+            self.active_bots.append(False)
+            
+    def poll_node_names(self):
+        #node_names is loaded from yaml file and should be a list of lists for each robot of desired nodes
+        #print(self.node_names)
+        for i in range(len(self.led_objects)):
+            if(self.active_bots[i]!=self.led_objects[i].active):
+                if(self.led_objects[i].active==True):
+                    out=pow(2,i)
+                    self.send_value+=out
+                    self.active_bots[i]=True
+                else:
+                    out=pow(2,i)
+                    self.send_value-=out
+                    self.active_bots[i]=False
+
+                output = Int32()
+                output.data=int(self.send_value)
+                self.publisher.publish(output)
+        time.sleep(0.01)
 
 
 class LEDIndicator(QAbstractButton):
