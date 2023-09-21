d5d4983000583ee042d6e8f0d6d8b10132fcfb83
Chukwuemeka-Ike <chukwuemeka.o.ike@gmail.com>
2023-09-21 03:39:42 -0400

    Functions for drawing changing schedule
    Added functions for drawing a schedule while taking into account ongoing tickets' behaviors. Formatted the timestamps to be HH:MM. Removed unnecessary imports and functions. Added job IDs as rectangles in the graph to bypass using yticks.

--- workspace/src/arm_utils/src/arm_utils/draw_utils.py
+++ workspace/src/arm_utils/src/arm_utils/draw_utils.py
@@ -4,29 +4,38 @@ ARM Project
 Author - Chukwuemeka Osaretin Ike
 
 Description:
-    Utilities for graphing, decorating, and saving the assignment and schedule.
+    Utilities for graphing the schedule.
 '''
-import glob
-import math
-import numpy as np
-import pandas as pd
-
-from matplotlib.animation import FFMpegWriter
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import pandas as pd
 
-# from arm_constants.machines import machine_type_indices, machine_type_abvs
+from matplotlib.dates import DateFormatter
 
 from arm_utils.conversion_utils import convert_schedule_to_task_list
 from arm_utils.job_utils import create_linear_jobs, get_job_subsizes
-from arm_utils.sched_utils import load_schedule
 
 
-def draw_rectangle(job_num: int, machine_type_num: int, machine_id: int,
+def draw_rectangle(
+        x: int, y: int, width: int, height: int, facecolor: str, ax: plt.axes
+    ) -> patches.Rectangle:
+    '''Draw a rectangle at the x-y location of the given width.'''
+    # Create the rectangle and add it to the axes object.
+    rectangle = patches.Rectangle(
+            (x, y), width,
+            height, linewidth=3.5,
+            edgecolor="#5e2222",
+            facecolor=facecolor
+        )
+    ax.add_patch(rectangle)
+    return rectangle
+
+def draw_ticket(job_num: int, machine_type_num: int, machine_id: int,
                    start: int, duration: int, ax: plt.axes,
-                   machine_type_indices: list, machine_type_abvs: list
-    ):
-    '''Draw a rectangle at the x-y location given by the start-job numbers.
+                   machine_type_indices: list, machine_type_abvs: list,
+                   fontsize: int
+    ) -> None:
+    ''' given by the start-job numbers.
 
     Args:
         job_num:
@@ -38,14 +47,9 @@ def draw_rectangle(job_num: int, machine_type_num: int, machine_id: int,
         machine_type_indices:
         machine_type_abvs:
     '''
-    # Create the rectangle and add it to the axes object.
-    rectangle = patches.Rectangle(
-            (start, job_num),
-            duration, 1, linewidth=3.5,
-            edgecolor="#5e2222",
-            facecolor=f"C{machine_type_num}"
-        )
-    ax.add_patch(rectangle)
+    rectangle = draw_rectangle(
+        start, job_num, duration, 1, f"C{machine_type_num}", ax
+    )
     rx, ry = rectangle.get_xy()
     cx = rx + rectangle.get_width()/2.0
     cy = ry + rectangle.get_height()/2.0
@@ -54,29 +58,76 @@ def draw_rectangle(job_num: int, machine_type_num: int, machine_id: int,
         f"{str(machine_type_indices[machine_id])}",
         (cx, cy),
         color='black', weight='bold',
-        fontsize=10, ha='center', va='center'
+        fontsize=fontsize, ha='center', va='center'
     )
 
-def draw_env(bounds: list, fontSize: int):
-    '''Draw the base environment for the schedule to be graphed.'''
-    plt.axis([bounds[0], bounds[1], bounds[2], bounds[3]])
-    numXTicks = 20
-    number = int(bounds[1]/numXTicks)
-    # print(bounds)
-    # print(number)
-    xSpacing = math.ceil(number/10)*10      # Round up to nearest 10.
-    # xSpacing = round(number, -1)            # Round to nearest 10.
-    # print(xSpacing)
-    plt.xticks(np.arange(bounds[0], bounds[1], xSpacing), fontsize=fontSize)
-    plt.yticks(np.arange(bounds[3], bounds[2], 1), fontsize=fontSize)
-    plt.xlabel('Time (mins)', fontsize=fontSize)
-    plt.ylabel('Job ID', fontsize=fontSize)
-    plt.grid()
+def draw_job_id(
+        x: int, y: int, width: int, height: int,
+        job_id: int, fontsize: int, ax: plt.axes
+    ) -> None:
+    '''.'''
+    rectangle = draw_rectangle(x, y, width, height, 'black', ax)
+    rx, ry = rectangle.get_xy()
+    cx = rx + rectangle.get_width()/2.0
+    cy = ry + rectangle.get_height()/2.0
+    ax.annotate(
+        f"{job_id}",
+        (cx, cy),
+        color='white', weight='bold',
+        fontsize=fontsize, ha='center', va='center'
+    )
 
-def draw_tree_schedule(schedule: pd.DataFrame, ax: plt.Axes,
-                    machine_type_indices: list, machine_type_abvs: list
-    ):
-    '''Draws a schedule including tree jobs.'''
+def draw_env(bounds: list, fontSize: int, ax: plt.axes) -> None:
+    '''Draw the base environment for the schedule to be graphed.'''
+    ax.axis([bounds[0], bounds[1], bounds[2], bounds[3]])
+
+    # Format to hour and minute only.
+    dfmt = DateFormatter("%H:%M")
+    ax.xaxis.set_major_formatter(dfmt)
+
+    # try:
+    ax.set_yticks([])
+    # except:
+    #     pass
+    ax.set_xlabel('Time', fontsize=fontSize)
+    ax.set_ylabel('Job', fontsize=fontSize)
+    ax.grid(axis='both')
+
+def draw_no_schedule(ax: plt.Axes, current_time: int) -> None:
+    '''Sets the bounds when there is no schedule to draw.'''
+    current_time = pd.to_datetime(current_time, unit='s')
+    maxY = 4
+    bounds = [
+        # (current_time - pd.Timedelta(hours=1)).to_pydatetime(),
+        # (current_time + pd.Timedelta(hours=7)).to_pydatetime(),
+        # Minutes for testing.
+        (current_time - pd.Timedelta(minutes=1)).to_pydatetime(),
+        (current_time + pd.Timedelta(minutes=7)).to_pydatetime(),
+        maxY, 0
+    ]
+    fontSize = 10
+
+    # Draw the base graph.
+    draw_env(bounds, fontSize, ax)
+    
+    # Draw the "now" line.
+    ax.axvline(x=current_time, color='r', linestyle='-', lw=5)
+
+    ax.fill_betweenx(
+        [maxY, 0],
+        bounds[0], current_time,
+        color='gray', alpha=0.5, label='Overlay')
+
+def draw_schedule(
+        schedule: pd.DataFrame,
+        ongoing: list,
+        machine_type_indices: list,
+        machine_type_abvs: list,
+        ax: plt.Axes,
+        current_time: int,
+        draw_full_schedule: bool=False
+    ) -> None:
+    '''Draws the schedule.'''
     # Bounds are jobs on y-axis, time index on x-axis. y-axis is flipped.
     # Each job on the y-axis may have more than one row.
     task_list = convert_schedule_to_task_list(schedule)
@@ -88,11 +139,33 @@ def draw_tree_schedule(schedule: pd.DataFrame, ax: plt.Axes,
     for i in range(num_jobs):
         maxY += sizes[i]
 
-    bounds = [0, schedule["end"].max()+1, maxY, 0]
-    fontSize = 15
+    schedule["start"] = pd.to_datetime(schedule["start"], unit='s')
+    schedule["end"] = pd.to_datetime(schedule["end"], unit='s')
+    current_time = pd.to_datetime(current_time, unit='s')
+
+    if not draw_full_schedule:
+        bounds = [
+            # (current_time - pd.Timedelta(hours=1)).to_pydatetime(),
+            # (current_time + pd.Timedelta(hours=7)).to_pydatetime(),
+            # Minutes for testing.
+            (current_time - pd.Timedelta(minutes=1)).to_pydatetime(),
+            (current_time + pd.Timedelta(minutes=7)).to_pydatetime(),
+            maxY, 0
+        ]
+        fontSize = 10
+    else:
+        bounds = [
+            schedule["start"].min().to_pydatetime(),
+            schedule["end"].max().to_pydatetime(),
+            maxY, 0
+        ]
+        fontSize = 10
+    # job_id_width = pd.Timedelta(minutes=6)
+    job_id_width = pd.Timedelta(seconds=15)
+    bounds[0] = bounds[0] - job_id_width
 
     # Draw the base graph.
-    draw_env(bounds, fontSize)
+    draw_env(bounds, fontSize, ax)
 
     # Draw rectangles for every task in the schedule.
     j = 0
@@ -100,169 +173,49 @@ def draw_tree_schedule(schedule: pd.DataFrame, ax: plt.Axes,
         job = schedule.loc[schedule["job_id"] == job_id]
         linear_jobs = create_linear_jobs(job, task_list)
 
-        for _, linear_job in enumerate(linear_jobs):
+        for linear_job in linear_jobs:
             for ticket_id in linear_job:
                 ticket = task_list[ticket_id]
-                draw_rectangle(
+                start_time = pd.to_datetime(ticket["start"], unit='s')
+                end_time = pd.to_datetime(ticket["end"], unit='s')
+                time_left = pd.Timedelta(ticket["time_left"], unit='s')
+
+                # If a ticket is not done, its time left is useful.
+                # If done, it's 0 and we use the relationship btw start & end.
+                if ticket_id in ongoing:
+                    duration = (current_time+time_left)-start_time
+                else:
+                    duration = end_time-start_time
+                draw_ticket(
                     j, ticket["machine_type"],
                     ticket["machine_id"],
-                    ticket["start"], ticket["duration"], 
+                    start_time,
+                    duration,
                     ax,
                     machine_type_indices,
-                    machine_type_abvs
+                    machine_type_abvs,
+                    fontSize
                 )
             j+=1
 
     # Draw horizontal lines separating each tree job.
     runTotal = 0
     for job_num in range(num_jobs):
+        draw_job_id(
+            bounds[0], runTotal, 
+            job_id_width,
+            sizes[job_num],
+            job_ids[job_num],
+            fontSize,
+            ax
+        )
         runTotal += sizes[job_num]
-        plt.axhline(y = runTotal, color = 'k', linestyle = '-', lw=5)
+        ax.axhline(y = runTotal, color='k', linestyle='-', lw=5)
 
-# def draw_tree_schedule(schedule: pd.DataFrame, scheduleFilename: str="Images/treeSched.png"):
-#     '''Draws a schedule including tree jobs.'''
-#     # Bounds are jobs on y-axis, time index on x-axis. y-axis is flipped.
-#     # Each job on the y-axis may have more than one row.
-#     task_list = convert_schedule_to_task_list(schedule)
-#     num_jobs = schedule["job_id"].nunique()
-#     job_ids = schedule["job_id"].unique()
-#     sizes = get_job_subsizes(schedule, task_list)
+    # Draw the "now" line.
+    ax.axvline(x=current_time, color='r', linestyle='-', lw=5)
 
-#     maxY = 0
-#     for i in range(num_jobs):
-#         maxY += sizes[i]
-
-#     bounds = [0, schedule["end"].max()+1, maxY, 0]
-#     fontSize = 20
-
-#     # Create the axes and draw the base graph.
-#     _, ax = plt.subplots(1, 1, figsize=(17,7))
-#     draw_env(bounds, fontSize)
-
-#     # Draw rectangles for every task in the schedule.
-#     j = 0
-#     for job_id in job_ids:
-#         job = schedule.loc[schedule["job_id"] == job_id]
-#         linear_jobs = create_linear_jobs(job, task_list)
-#         # print(linear_jobs)
-
-#         for _, linear_job in enumerate(linear_jobs):
-#             for ticket_id in linear_job:
-#                 ticket = task_list[ticket_id]
-#                 draw_rectangle(
-#                     j, ticket["machine_type"],
-#                     ticket["machine_id"],
-#                     ticket["start"], ticket["duration"], 
-#                     ax
-#                 )
-#             j+=1
-
-#     # Draw horizontal lines separating each tree job.
-#     runTotal = 0
-#     for job_num in range(num_jobs):
-#         runTotal += sizes[job_num]
-#         plt.axhline(y = runTotal, color = 'k', linestyle = '-', lw=5)
-#     # plt.savefig(scheduleFilename)
-#     # plt.show()
-
-def draw_evolving_schedule(search_dir: str, saveFilename: str="animatedSched.mp4"):
-    '''Draws a schedule including tree jobs.'''
-    # Get the number of schedule files and load the save times for each.
-    search_word = "schedule"
-    file_paths = [file for file in glob.glob(search_dir + '/*' + search_word + '*')]
-    num_scheds = len(file_paths)
-    print(num_scheds)
-
-    with open(f"{search_dir}/sched_times.txt", 'r') as f:
-        times = [float(line.strip()) for line in f.readlines()]
-    times = [t - times[0] for t in times]
-
-    # Load the actual executed schedule.
-    schedule = load_schedule(f"{search_dir}/savedSched.csv")
-    min_time = schedule["start"].min()
-    schedule["end"] = schedule["end"] - min_time
-    actual_schedule_end = schedule["end"].max()+1
-
-    # Create the axes and draw the base graph.
-    fig, ax = plt.subplots(1, 1, figsize=(17,7))
-    fontSize = 20
-
-    metadata = dict(
-        title=f"Evolving schedule.")
-    writer = FFMpegWriter(fps=0.5, metadata=metadata)
-
-    with writer.saving(fig, saveFilename, 100):
-        for i in range(num_scheds):
-            # print(i)
-            schedule = load_schedule(f"{search_dir}/schedule{i+1}.csv")
-            schedule["start"] = schedule["start"] + times[i]
-            schedule["end"] = schedule["end"] + times[i]
-            schedule["duration"] = schedule.apply(lambda x: x["end"]-x["start"], axis=1)
-
-            task_list = convert_schedule_to_task_list(schedule)
-
-            num_jobs = schedule["job_id"].nunique()
-            job_ids = schedule["job_id"].unique()
-            sizes = get_job_subsizes(schedule, task_list)
-            bounds = [0, actual_schedule_end, sum(sizes), 0]
-            # print(bounds)
-            # print(schedule)
-
-            # Draw rectangles for every task in the schedule.
-            draw_env(bounds, fontSize)
-            j = 0
-            for job_id in job_ids:
-                job = schedule.loc[schedule["job_id"] == job_id]
-                linear_jobs = create_linear_jobs(job, task_list)
-                # print(linear_jobs)
-
-                for _, linear_job in enumerate(linear_jobs):
-                    for ticket_id in linear_job:
-                        ticket = task_list[ticket_id]
-                        draw_rectangle(
-                            j, ticket["machine_type"],
-                            ticket["machine_id"],
-                            ticket["start"], ticket["time_left"], 
-                            ax
-                        )
-                    j+=1
-
-            # Draw horizontal lines separating each tree job.
-            runTotal = 0
-            for job_num in range(num_jobs):
-                runTotal += sizes[job_num]
-                plt.axhline(y = runTotal, color = 'k', linestyle = '-', lw=5)
-            plt.axvline(x = times[i], color = 'k', linestyle = '-', lw=5)
-            plt.title(f"Schedule at T = {times[i]}.", fontsize = fontSize)
-            plt.draw()
-
-            writer.grab_frame()
-
-            if i < num_scheds - 1:
-                ax.clear()
-
-        # Add a frame, so the video doesn't end abruptly on the last update.
-        writer.grab_frame()
-
-def draw_labor_schedule(labor_schedule: pd.DataFrame, all_robots: list, scheduleFilename: str="Images/laborSched.png"):
-    '''Draws the schedule for each labor unit.'''
-    num_robots = len(all_robots)
-    bounds = [0, labor_schedule["end"].max()+1, num_robots, 0]
-    fontSize = 20
-
-    # Create the axes and draw the base graph.
-    _, ax = plt.subplots(1, 1, figsize=(17,7))
-    draw_env(bounds, fontSize)
-
-    for robot in range(num_robots):
-        robot_assigned = labor_schedule.loc[labor_schedule["robot_num"] == robot]
-        for assignment in range(len(robot_assigned)):
-            row = robot_assigned.iloc[assignment]
-            draw_rectangle(
-                robot, row["machine_type"],
-                row["ticket_id"],
-                row["start"], row["duration"],
-                ax
-            )
-    plt.savefig(scheduleFilename)
-    # plt.show()
+    ax.fill_betweenx(
+        [maxY, 0],
+        bounds[0], current_time,
+        color='gray', alpha=0.5, label='Overlay')
